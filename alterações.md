1a26ede7a408b02e0a5a3715c46953680882eb7c
gonCSgit <103418696+gonCSgit@users.noreply.github.com>
2022-04-11 13:24:13 +0100

    Comentários e index rename

--- alterações.md
+++ alterações.md
@@ -1,6 +1,12 @@
 ## Gonçalo
 
-> mover o script para o head do HTML
+> mover o script para o head do HTML;
+> linha 39 não retorna setGameOver();
+> checkGuess() aceita strings em guessField mas não retorna avisos/erros
+> guessesObj.tries += 1; encontra-se fora da função checkGuess()
+> função de setGameOver() parece criar um reset button sem classes ou ID
+> função resetGame() parece limpar o guesses object mas este deveria permanecer ao longo de vários jogos
+> funão resetGame() está declarada mas não parece ter nenhum click event associado
 
 ## Luís
 
--- app.ts
+++ app.ts
@@ -1,12 +1,15 @@
+//Store random number in variable
 let randomNumber = Math.floor(Math.random() * 100) + 1;
 console.log(randomNumber);
 
+//Selectors
 const guesses = document.querySelector(".guesses");
 const lastResult = document.querySelector(".lastResult");
 const lowOrHi = document.querySelector("lowOrHi");
 const guessSubmit = document.querySelector(".guessSubmit");
 const guessField = document.querySelector(".guessField");
 
+//checkGuess() and setGameOver() components
 let guessCount = 1;
 let resetButton;
 let guessesObj = {
@@ -14,24 +17,29 @@ let guessesObj = {
 };
 
 const checkGuess = () => {
-  const userGuess = Number(guessField.value);
+  const userGuess = Number(guessField.value); //Turn input into number
+  //If guessCount is equal to one show previous guesses?
   if (guessCount === 1) {
     guesses.textContent = "Previous guesses: ";
   }
-  guesses.textContent += userGuess + " ";
+  guesses.textContent += userGuess + " "; //Increment last user guess to text field
 
+  //Win condition. Returns setGameOver() state
   if ((userGuess = randomNumber)) {
     lastResult.textContent = "Congratulations! You got it right!";
     lastResult.style.backgroundColor = "green";
     lowOrHi.textContent = "";
     return setGameOver();
   }
+
+  //Loss condition. Should return setGameOver()?
   if (guessCount === "3") {
     lastResult.textContent = "!!!GAME OVER!!!";
     return;
     setGameOver();
   }
 
+  //Else conditions. Used if user input was never the correct answer or guess count is still below 3.
   lastResult.textContent = "Wrong!";
   lastResult.style.backgroundColor = "red";
   if (userGuess < randomNumber) {
@@ -40,12 +48,15 @@ const checkGuess = () => {
     lowOrHi.textContent = "Last guess was too high!";
   }
 
+  //Clear guess field and focus
   guessField.value = "";
   guessField.focus();
 };
 guessSubmit.addeventListener("click", checkGuess);
 
-guessesObj.tries += 1;
+guessesObj.tries += 1; //Increment guesses in object ?
+
+//GameOver state will disable input fields, create reset button and append it to ??? with ??? classes
 const setGameOver = () => {
   guessField.disabled = true;
   guessSubmit.disabled = true;
@@ -53,6 +64,7 @@ const setGameOver = () => {
   resetButton.textContent = "Start new game";
   document.body.appendChild(resetButton);
 
+  //resetGame() will select result parameters by class, clear their text fields, remove reset button and set variables to initial values ?
   const resetGame = () => {
     const resetParas = document.querySelectorAll(".resultParas p");
     for (const resetPara of resetParas) {
